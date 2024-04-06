0047c950dbab814da2b5495fed74b2c88a92ea7c
SirknightMike <combrim@gmail.com>
2024-04-06 21:22:56 +0200

    feat(profile): updated the profile page wip

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+export interface Profile {
+  FirstName: string;
+  LastName: string;
+  ChampionshipTitles: number;
+  Victories: number;
+  WinPercentage: number;
+}
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent {
-
+  profile: Profile = {
+    FirstName: "Michael",
+    LastName: "Petterson",
+    ChampionshipTitles: 2,
+    Victories: 50,
+    WinPercentage: 60
+  }
 }
